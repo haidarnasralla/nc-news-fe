adbf0e8ab77f7f781a0dd3037f3deea76d942018
Haidar Nasralla <heidernasralla@gmail.com>
2024-11-13 15:06:06 +0000

    Removed LinkedIn link

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,7 +7,6 @@ const Header = () => {
             <Link to={'/'}>
                 <h1>NC News</h1>
             </Link>
-            <h2><a href="https://www.linkedin.com/in/haidarnasralla/LinkedIn">LinkedIn</a></h2>
         </>
     )
 }
